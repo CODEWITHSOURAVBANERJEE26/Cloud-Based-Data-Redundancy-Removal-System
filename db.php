422265d5b2fd4628cf1c330eda05c2f1410281e4
CODEWITHSOURAVBANERJEE26 <147583018+CODEWITHSOURAVBANERJEE26@users.noreply.github.com>
2026-06-13 11:41:54 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$database="redundancy_db";
+
+$conn=mysqli_connect(
+$host,
+$user,
+$password,
+$database
+);
+
+if(!$conn)
+{
+die("Database Connection Failed");
+}
+
+?>
