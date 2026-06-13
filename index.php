d8e3c3ef19f1da290de04d2b98517893e823e8c1
CODEWITHSOURAVBANERJEE26 <147583018+CODEWITHSOURAVBANERJEE26@users.noreply.github.com>
2026-06-13 11:39:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>
+Cloud-Based Data Redundancy Removal System
+</title>
+
+<link rel="stylesheet"
+href="style.css">
+
+</head>
+
+<body>
+
+<h1>
+Cloud-Based Data Redundancy Removal System
+</h1>
+
+<form action="insert.php"
+method="POST">
+
+<input
+type="text"
+name="name"
+placeholder="Enter Name"
+required>
+
+<br><br>
+
+<input
+type="email"
+name="email"
+placeholder="Enter Email"
+required>
+
+<br><br>
+
+<input
+type="text"
+name="phone"
+placeholder="Enter Phone"
+required>
+
+<br><br>
+
+<input
+type="submit"
+value="Save Record">
+
+</form>
+
+</body>
+</html>
